c6a97062f9a52fb469b1b4983e32987c6fa632c0
alex <alex@sinus.fritz.box>
2023-08-05 22:50:33 +0200

    fixed bug #728 (Unable to create iso image on FreeBSD)

--- src/mediaenc_ffmpeg.cpp
+++ src/mediaenc_ffmpeg.cpp
@@ -330,7 +330,9 @@ bool wxFfmpegMediaEncoder::addAudioStream(int codecId) {
 		return false;
 	for (int i = 0; i < c->channels; i++) {
 		uint16_t *samples = (uint16_t*)m_audioFrame->data[i];
+		if (samples) {
 		memset(samples, 0, c->frame_size * av_get_bytes_per_sample(c->sample_fmt));
+		}
 	}
 
 	return true;
